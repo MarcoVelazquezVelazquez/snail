cfcd6f2d237d1f4c67112392215060b748dc0fd9
Dinara Dulanova <dulanovad@mail.ru>
2021-07-09 06:44:01 +0000

    Update Snail.java

--- src/main/java/com/epam/rd/autotasks/snail/Snail.java
+++ src/main/java/com/epam/rd/autotasks/snail/Snail.java
@@ -6,8 +6,7 @@ public class Snail
     {
         //Write a program that reads a,b and h (line by lyne in this order) and prints
         //the number of days for which the snail reach the top of the tree.
-        //a - feet that snail travels up each day, b - feet that slides down each night,
-        // h - height of the tree
+        //a - feet that snail travels up each day, b - feet that slides down each night, h - height of the tree
 
     }
 }
