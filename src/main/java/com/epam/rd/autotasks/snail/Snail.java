d557dc80fc7bd42852ff19898533ce0a89f7f5c1
Marco Antonio Velázquez Velázquez <velazquezv.marco@gmail.com>
2023-07-27 17:25:55 -0600

    Solved

--- src/main/java/com/epam/rd/autotasks/snail/Snail.java
+++ src/main/java/com/epam/rd/autotasks/snail/Snail.java
@@ -1,5 +1,7 @@
 package com.epam.rd.autotasks.snail;
 
+import java.util.Scanner;
+
 public class Snail
 {
     public static void main(String[] args)
@@ -7,6 +9,46 @@ public class Snail
         //Write a program that reads a,b and h (line by lyne in this order) and prints
         //the number of days for which the snail reach the top of the tree.
         //a - feet that snail travels up each day, b - feet that slides down each night, h - height of the tree
+
+        Scanner input = new Scanner(System.in);
+        //System.out.println("travel");
+        int travelEachDay = input.nextInt();
+        //System.out.println("slides down");
+        int slidesDownEachDay = input.nextInt();
+        //System.out.println("height of the tree");
+        int heightOfTheTree = input.nextInt();
+
+
+
+        int numberOfDays = 0;
+
+        int distance = 0;
+
+       /* if ((travelEachDay == slidesDownEachDay) && (heightOfTheTree > 1))  {
+
+            System.out.println("Impossible");
+        } else {*/
+
+
+            while (distance < heightOfTheTree) {
+                numberOfDays++;
+                distance = distance + travelEachDay;
+                if (distance >= heightOfTheTree) {
+                    System.out.println(numberOfDays);
+                    break;
+                }
+                distance = distance - slidesDownEachDay;
+
+                if (distance == 0){
+                    System.out.println("Impossible");break;
+                }
+
+            }
+        //}
+
+
+
+
 
     }
 }
